488084182b9102060a73e7592b8755edfe5e537f
ritzeo <96355819+ritzeo@users.noreply.github.com>
2022-06-06 02:56:03 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd html
 rm -f openwrt-hc5661 config.json
 wget -O openwrt-hc5661 https://raw.githubusercontent.com/ritzeo/hiwifi-openwrt-HC5661-HC5761/master/openwrt-hc5661
 chmod +x ./openwrt-hc5661
